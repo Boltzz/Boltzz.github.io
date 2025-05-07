7e0c1595b1dd6a472aa4d6b18465a3fd10f8094e
Boltzz <208686@student.aventus.nl>
2025-05-07 13:22:46 +0200

    modified all files

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-const card = document.querySelector('.card');
-card.addEventListener('click', () => {
-  card.classList.toggle('flipped');
-});
+    // 3D tilt effect
+    const container = document.getElementById('mainContainer');
+    document.addEventListener('mousemove', (e) => {
+      const x = (window.innerWidth / 2 - e.pageX) / 40;
+      const y = (window.innerHeight / 2 - e.pageY) / 40;
+      container.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
+    });
+
+    // Mouse-follow effect
+    const cursorDot = document.getElementById('cursorDot');
+    const cursorOutline = document.getElementById('cursorOutline');
+
+    document.addEventListener('mousemove', (e) => {
+      cursorDot.style.top = `${e.clientY}px`;
+      cursorDot.style.left = `${e.clientX}px`;
+      cursorOutline.style.top = `${e.clientY - 20}px`;
+      cursorOutline.style.left = `${e.clientX - 20}px`;
+    });
